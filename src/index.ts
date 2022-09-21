b7ff46228737fed08ba5b54b857a3648a0538d31
moinnawaz96 <fearyaakuza@gmail.com>
2022-09-21 23:00:13 +0200

    test

--- src/index.ts
+++ src/index.ts
@@ -6,5 +6,5 @@ const OAuth2Client = registerPlugin<OAuth2ClientPlugin>('OAuth2Client', {
     web: () => import('./web').then(m => new m.OAuth2ClientPluginWeb()),
 });
 
-export * from './definitions';
+export * from "./definitions";
 export { OAuth2Client };
